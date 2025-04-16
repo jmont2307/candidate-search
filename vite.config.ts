af753f35b31f3fa10022b6228c7b3a70fde0b7c8
jmont2307 <jh.montalvoiv@gmail.com>
2025-04-15 21:04:08 -0600

    Update Vite config to allow Render host

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
+  envDir: './environment',
   plugins: [react()],
+  preview: {
+    port: 4173,
+    strictPort: true,
+    allowedHosts: ['candidate-search-mb9v.onrender.com', '*.onrender.com']
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+  }
 });
